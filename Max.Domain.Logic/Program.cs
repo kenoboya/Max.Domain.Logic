5408999e0b0ce0d6d264b19232ba0648a3ab4d10
Max Mazur <flash23100@gmail.com>
2024-09-04 13:10:00 +0300

    Add project files.

---
+++ Max.Domain.Logic/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Max.Domain.Logic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
